00fe68358eb5fd232cbe632a4a59b2951758d0df
Marijn-1999 <marijn1999@home.nl>
2026-03-18 13:59:11 +0100

    Idle animations added

--- Marijn/player.gd
+++ Marijn/player.gd
@@ -2,17 +2,19 @@ extends CharacterBody2D
 
 @export var speed = 100
 
+# Track the last logical direction for idle
+# Can be: "Up", "Down", "Left", "Right", "UpRight", "UpLeft", "DownRight", "DownLeft"
+var last_direction = "Down"
+
 func _physics_process(delta):
 	var x = 0
 	var y = 0
 
-	# Horizontal input
+	# --- Input ---
 	if Input.is_action_pressed("move_right"):
 		x += 1
 	if Input.is_action_pressed("move_left"):
 		x -= 1
-
-	# Vertical input
 	if Input.is_action_pressed("move_down"):
 		y += 1
 	if Input.is_action_pressed("move_up"):
@@ -21,37 +23,49 @@ func _physics_process(delta):
 	var input_vector = Vector2(x, y)
 
 	if input_vector != Vector2.ZERO:
+		# --- Movement ---
 		input_vector = input_vector.normalized()
 		velocity = input_vector * speed
 		move_and_slide()
 
-		# --- Animation selection ---
-		if x > 0 and y < 0:  # UpRight
-			$AnimatedSprite2D.animation = "UpRight"
-			$AnimatedSprite2D.flip_h = false
-		elif x < 0 and y < 0:  # UpLeft
-			$AnimatedSprite2D.animation = "UpRight"  # reuse Right sprite
-			$AnimatedSprite2D.flip_h = true
-		elif x > 0 and y > 0:  # DownRight
-			$AnimatedSprite2D.animation = "DownRight"
-			$AnimatedSprite2D.flip_h = false
-		elif x < 0 and y > 0:  # DownLeft
-			$AnimatedSprite2D.animation = "DownRight"  # reuse Right sprite
-			$AnimatedSprite2D.flip_h = true
-		elif y < 0:  # Up
-			$AnimatedSprite2D.animation = "Up"
-			$AnimatedSprite2D.flip_h = false
-		elif y > 0:  # Down
-			$AnimatedSprite2D.animation = "Down"
-			$AnimatedSprite2D.flip_h = false
-		elif x > 0:  # Right
-			$AnimatedSprite2D.animation = "Right"
-			$AnimatedSprite2D.flip_h = false
-		elif x < 0:  # Left
-			$AnimatedSprite2D.animation = "Right"
-			$AnimatedSprite2D.flip_h = true
+		# --- Determine walking animation ---
+		var anim_name = ""
+		var flip_h = false
 
+		if x > 0 and y < 0:
+			anim_name = "UpRight"
+			last_direction = "UpRight"
+		elif x < 0 and y < 0:
+			anim_name = "UpRight"  # mirrored
+			flip_h = true
+			last_direction = "UpLeft"
+		elif x > 0 and y > 0:
+			anim_name = "DownRight"
+			last_direction = "DownRight"
+		elif x < 0 and y > 0:
+			anim_name = "DownRight"  # mirrored
+			flip_h = true
+			last_direction = "DownLeft"
+		elif y < 0:
+			anim_name = "Up"
+			last_direction = "Up"
+		elif y > 0:
+			anim_name = "Down"
+			last_direction = "Down"
+		elif x > 0:
+			anim_name = "Right"
+			last_direction = "Right"
+		elif x < 0:
+			anim_name = "Right"  # mirrored
+			flip_h = true
+			last_direction = "Left"
+
+		$AnimatedSprite2D.animation = anim_name
+		$AnimatedSprite2D.flip_h = flip_h
 		$AnimatedSprite2D.play()
 	else:
+		# --- Idle animation based on last logical direction ---
 		velocity = Vector2.ZERO
-		$AnimatedSprite2D.stop()
+		$AnimatedSprite2D.animation = "Idle" + last_direction
+		$AnimatedSprite2D.flip_h = false  # idle sprites already exist for left
+		$AnimatedSprite2D.play()

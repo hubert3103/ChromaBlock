93115aa53dd7fa616c17d518dcb2bc4674fda239
Marijn-1999 <marijn1999@home.nl>
2026-03-27 11:28:32 +0100

    Added functioning door
    Added a functioning door that opens when a block is pushed onto the switch on the floor.

--- Marijn/Level1/floor_switch.gd
+++ Marijn/Level1/floor_switch.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
 @export var required_id: String = "A"
+@export var unpressed_texture: Texture
+@export var pressed_texture: Texture
+@export var door_node: NodePath  # assign your Door node here in the editor
 
 var is_active = false
 
@@ -16,8 +19,20 @@ func _on_body_exited(body: Node2D) -> void:
 
 func activate():
 	print("Switch ON")
-	
+	if $Sprite2D:
+		$Sprite2D.texture = pressed_texture
+	# Tell the door to open (hide sprite + disable hitbox)
+	if door_node:
+		var door = get_node(door_node)
+		if door:
+			door.call_deferred("open")  # use deferred to avoid physics flush errors
 
 func deactivate():
 	print("Switch OFF")
-	
+	if $Sprite2D:
+		$Sprite2D.texture = unpressed_texture
+	# Tell the door to close (show sprite + enable hitbox)
+	if door_node:
+		var door = get_node(door_node)
+		if door:
+			door.call_deferred("close")

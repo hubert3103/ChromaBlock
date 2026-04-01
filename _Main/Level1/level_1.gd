c6f528e7a367bf3984da0f919408dc8827e75b3b
Marijn-1999 <marijn1999@home.nl>
2026-04-01 14:44:50 +0200

    Updated the HUD

--- _Main/Level1/level_1.gd
+++ _Main/Level1/level_1.gd
@@ -1,4 +1,14 @@
 extends Node2D
 
+@export var hud_display_time := 2.0  # seconds
+
 func _ready():
+	# Position player at start
 	$Player.position = $StartPosition.position
+	
+	# Show the StartHUD
+	$StartHUD.visible = true
+	
+	# Wait for display_time seconds, then hide it
+	await get_tree().create_timer(hud_display_time).timeout
+	$StartHUD.visible = false

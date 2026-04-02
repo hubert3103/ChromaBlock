15ea4821a3882d9b34158a988df5b0ba4650398a
hubert3103 <hubert3103@gmail.com>
2026-04-02 13:44:22 +0200

    Switch node finished

--- hubert/switch.gd
+++ hubert/switch.gd
@@ -1,44 +1,55 @@
 extends Node2D
 
-signal switched_on
-signal switched_off
+@export var unpressed_texture: Texture
+@export var pressed_texture: Texture
+@export var door_node: NodePath  
 
-@export var door: Node2D
+var is_active = false
 var player_in_range = false
-var is_on = false
-
-func _ready():
-	switched_on.connect(_on_switched_on)
-	switched_off.connect(_on_switched_off)
-
-func _on_area_2d_body_entered(body):
-	if body.is_in_group("player"):
-		player_in_range = true
-
-func _on_area_2d_body_exited(body):
-	if body.is_in_group("player"):
-		player_in_range = false
 
 func _process(_delta):
 	if player_in_range and Input.is_action_just_pressed("interact"):
 		toggle()
 
+
 func toggle():
-	is_on = !is_on
-
-	if is_on:
-		if has_node("AnimationPlayer"):
-			$AnimationPlayer.play("switch_on")
-		emit_signal("switched_on")
+	is_active = !is_active
+	
+	if is_active:
+		activate()
 	else:
-		if has_node("AnimationPlayer"):
-			$AnimationPlayer.play("switch_off")
-		emit_signal("switched_off")
+		deactivate()
 
-func _on_switched_on():
-	if door and door.has_method("open"):
-		door.open()
+func activate():
+	print("Switch ON")
+	
+	if $Sprite2D:
+		$Sprite2D.texture = pressed_texture
+	
+	if door_node:
+		var door = get_node(door_node)
+		if door:
+			door.call_deferred("open")
 
-func _on_switched_off():
-	if door and door.has_method("close"):
-		door.close()
+func deactivate():
+	print("Switch OFF")
+	
+	if $Sprite2D:
+		$Sprite2D.texture = unpressed_texture
+	
+	if door_node:
+		var door = get_node(door_node)
+		if door:
+			door.call_deferred("close")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = true
+		print("Player in range")
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = false
+		print("Player left range")

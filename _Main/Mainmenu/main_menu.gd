5e7174f4228bff50f342c7bb2176df27eac9dc62
Marijn-1999 <marijn1999@home.nl>
2026-04-23 16:22:39 +0200

    Connected buttons on main menu

--- _Main/Mainmenu/main_menu.gd
+++ _Main/Mainmenu/main_menu.gd
@@ -19,3 +19,7 @@ func _on_start_button_pressed() -> void:
 func _on_exit_button_pressed() -> void:
 	get_tree().quit() 
 	print("Exit button clicked")
+
+
+func _on_how_to_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://_Main/HowToPlay/how_to_play.tscn")
